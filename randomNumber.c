5bcb46e01dd28b2436ed9b77744bde6275391497
AbuZuba1r <133815527+Evrcld@users.noreply.github.com>
2024-01-25 00:01:51 -0600

    Add files via upload

---
+++ randomNumber.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
+int main()
+{
+    srand(time(0));
+    int number = rand() %100;
+    int entered;
+    int attempts = 0;
+
+    printf("Guess the number: ");
+
+    do
+    {   
+        scanf("%d", &entered);
+
+        if (entered < number)
+        {
+            printf("Guess higher\n");
+        }
+
+        else if (entered > number)
+        {
+            printf("Guess lower\n");
+        }
+
+        attempts++;
+        
+    } while (entered != number);
+
+    printf("**************\n");
+    printf("CORRECT!\n");
+    printf("It took you : %d attempts\n", attempts);
+    printf("**************\n");
+
+    
+
+    
+    
+
+
+
+
+    return 0;
+}
